e52c1ba5bd455c761bdc0e2263d858706533d06a
Jefferson <limajeff00@gmail.com>
2026-05-13 12:09:12 -0400

    Integra cadastro de assistidos ao banco de dados

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Assistido
 
 def cadastro_cliente_view(request):
     return render(request, 'clientes/cadastro.html')
@@ -8,6 +9,20 @@ def portal_aluno_view(request):
     return render(request, 'clientes/portal_aluno.html')
 
 def novo_assistido_view(request):
+    if request.method == 'POST':
+        Assistido.objects.create(
+            nome_completo=request.POST.get('nome_completo'),
+            cpf=request.POST.get('cpf'),
+            rg=request.POST.get('rg'),
+            nacionalidade=request.POST.get('nacionalidade'),
+            profissao=request.POST.get('profissao'),
+            endereco=request.POST.get('endereco'),
+            telefone=request.POST.get('telefone'),
+            whatsapp=request.POST.get('whatsapp'),
+            email=request.POST.get('email'),
+        )
+
+        return redirect('portal_aluno')
 
     return render(request, 'clientes/novo_assistido.html')
 
