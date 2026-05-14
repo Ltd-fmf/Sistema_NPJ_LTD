cd3511d7206c93082d70ff1c51ce7bdbcb202053
Jefferson <limajeff00@gmail.com>
2026-05-14 15:30:48 -0400

    Reorganiza estrutura do repositório

--- Documents/Sistema_NPJ_LTD/atendimentos/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Atendimento
-
-admin.site.register(Atendimento)
--- Documents/Sistema_NPJ_LTD/atendimentos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AtendimentosConfig(AppConfig):
-    name = 'atendimentos'
